da52c04d99faf81983e31bb5be7ef4dce9899b31
wenderson-juvenal <wendersjuv@gmail.com>
2024-10-17 19:15:37 -0300

    remoção de: setas em projects, menu, imagem em about. Troca de background em algumas seções e algunas animações

--- script.js
+++ script.js
@@ -3,31 +3,6 @@ setTimeout(() => {
     loader.style.display = "none"
 }, 500)
 
-// eventos no nav dos projetos
-
-let previousButton = document.querySelector("#previous-button")
-let nextButton = document.querySelector("#next-button")
-let project = document.querySelector(".project")
-let divProjects = document.querySelector("#projects div")
-
-nextButton.addEventListener("click", () => {
-    divProjects.scrollLeft += project.clientWidth + 10
-})
-previousButton.addEventListener("click", () => {
-    if (Math.abs(divProjects.scrollLeft - (divProjects.scrollWidth - divProjects.clientWidth)) <= 1) {
-        let sub
-        for (let i=0; i < divProjects.clientWidth; i += project.clientWidth + 10) {
-            sub = project.clientWidth - (divProjects.clientWidth - i) 
-        }
-        divProjects.scrollLeft -= sub
-    } else {
-        divProjects.scrollLeft -= project.clientWidth + 10
-    }
-    
-})
-
-// botao mostrar mais em tecnologias de cada projeto
-
 document.addEventListener("DOMContentLoaded", function() {
     let listTechs = document.querySelectorAll(".techs")
     listTechs.forEach(list => {
